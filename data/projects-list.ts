9683d3c738d13bb8c3e4518ab984942c7d51a1a8
Ercan Ünal <ercanunal@anayurtteknoloji.com>
2021-11-24 12:04:01 +0300

    ercello

--- data/projects-list.ts
+++ data/projects-list.ts
@@ -6,50 +6,59 @@
 
 export const projectsList = [
   {
+    id: 0,
+    title: "ercello",
+    blurHash: "L0Aer?tjH[tPyAayj[j[11ay%xkB",
+    logo: "/assets/images/projects/trello.png",
+    link: "https://ercello.vercel.app",
+    desc: `A Trello Clone built using Typescript and React.`,
+    technologies: ["typescript", "react"],
+  },
+  {
     id: 1,
     title: "urcode",
     blurHash: "L0Aer?tjH[tPyAayj[j[11ay%xkB",
-    logo: '/assets/images/projects/urcode.png',
+    logo: "/assets/images/projects/urcode.png",
     link: "https://urcode.link",
     desc: `The urcode consists of several categories. Each category is labeled with a letter and some qualifiers. Go through each category and determine which set of qualifiers best describes you in that category. By selecting the right qualifiers and submitting this form, you are able to construct your overall urcode.
     Some of the qualifiers will very probably not match with you exactly. It is impossible to cover all possibilities in each category. Simply choose that qualifier that most closely matches you. Each description of each qualifier describes the wide range of activities that apply, so as long as you match with one, you can probably use that qualifier.`,
-    technologies: ["nextjs", "pwa", "javascript", "react"]
+    technologies: ["nextjs", "pwa", "javascript", "react"],
   },
   {
     id: 2,
     title: "balkabağı",
     link: "https://play.google.com/store/apps/details?id=com.balkabagii&hl=tr&gl=US",
-    logo: '/assets/images/projects/balkabagi.png',
+    logo: "/assets/images/projects/balkabagi.png",
     blurHash: "L0Aer?tjH[tPyAayj[j[00ay%xkB",
     desc: ` A react native mobile application that informs users about curfew period. `,
-    technologies: ["react native", "javascript"]
+    technologies: ["react native", "javascript"],
   },
   {
     id: 3,
     title: "erchat",
-    logo: '/assets/images/projects/erchat.png',
+    logo: "/assets/images/projects/erchat.png",
     blurHash: "L2M5%%0000EN00I:-oIp00kC?^sl",
     link: "https://github.com/3rco/erchat",
     desc: `Real time chat application made with html,css,javascript,socket.io`,
-    technologies: ["javascript", "socket"]
+    technologies: ["javascript", "socket"],
   },
   {
     id: 4,
     title: "niminy",
-    logo: '/assets/images/projects/niminy.png',
+    logo: "/assets/images/projects/niminy.png",
     blurHash: "L1O^uE0679I00AGT?.H{00y7p2tK",
     link: "https://marketplace.visualstudio.com/items?itemName=niminy.niminy",
     desc: `niminy is an extension that includes a number of functions to increase productivity. niminy is counting down from the selected duration and warns the user when the time is up.`,
-    technologies: ["javascript", "vscode extension"]
+    technologies: ["javascript", "vscode extension"],
   },
   {
     id: 5,
     title: "map pinner",
     // logo: gosave,
-    logo: '/assets/images/projects/mappinner.png',
+    logo: "/assets/images/projects/mappinner.png",
     blurHash: "L09sWSxdH;s?%6axWAfk00V?%jax",
     link: "https://map-demo-e4aa6.web.app/",
     desc: `Interactive map application using react and openstreetmap api `,
-    technologies: ["react", "openstreetmap", "firebase"]
-  }
+    technologies: ["react", "openstreetmap", "firebase"],
+  },
 ];
